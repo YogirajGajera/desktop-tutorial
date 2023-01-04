f17355fd3ef4b261e2c7354e1f08a354a7c83791
YogirajGajera <118765042+YogirajGajera@users.noreply.github.com>
2023-01-04 16:17:08 +0530

    Add files via upload

---
+++ App 1/App/App/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by mac on 04/01/23.
+//  Copyright © 2023 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lbl_name: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func btn_hide(_ sender: UIButton) {
+       sender.buttonType
+    }
+    
+}
+
